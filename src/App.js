14d9c5c1b021b59f68c032622808acf6a2dd4b71
Stephen Connett <StephenCon@users.noreply.github.com>
2023-09-25 16:08:17 +0100

    added comments

--- src/App.js
+++ src/App.js
@@ -7,25 +7,32 @@ import MealDisplay from './components/MealDisplay/MealDisplay';
 import RandomiserButton from './components/RandomiserButton/RandomiserButton';
 
 const App = () => {
+  // State to store the list of recipes
   const [recipes, setRecipes] = useState([]);
+
+  // State to store the randomized meals (breakfast, lunch, dinner)
   const [randomMeals, setRandomMeals] = useState({ breakfast: null, lunch: null, dinner: null });
 
+  // Handler to add a new recipe to the recipes list
   const handleAddRecipe = (newRecipe) => {
     setRecipes([...recipes, newRecipe]);
   };
 
+  // Handler to delete a recipe from the recipes list
   const handleDelete = (index) => {
     const updatedRecipes = [...recipes];
     updatedRecipes.splice(index, 1);
     setRecipes(updatedRecipes);
   };
 
+  // Handler to edit an existing recipe in the recipes list
   const handleEdit = (index, updatedRecipe) => {
     const updatedRecipes = [...recipes];
     updatedRecipes[index] = updatedRecipe;
     setRecipes(updatedRecipes);
   };
 
+  // Handler to update the state with randomized meals
   const handleRandomizedMeals = (randomizedMeals) => {
     setRandomMeals(randomizedMeals);
   };
@@ -33,9 +40,13 @@ const App = () => {
   return (
     <div className="container mt-5">
       <h1 className="text-center mb-4">Meal Mixer</h1>
+      {/* Recipe form to add a new recipe */}
       <RecipeForm onAddRecipe={handleAddRecipe} />
+      {/* List display of all recipes with options to edit or delete */}
       <RecipeList recipes={recipes} onDeleteRecipe={handleDelete} onEditRecipe={handleEdit} />
+      {/* Button to trigger the randomization of meals */}
       <RandomiserButton recipes={recipes} onRandomize={handleRandomizedMeals} />
+      {/* Display the randomized meals */}
       <MealDisplay meals={randomMeals} />
     </div>
   );
