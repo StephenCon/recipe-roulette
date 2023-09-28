25ea8c3b47832d3a8898cf6e0c8c2722d3dbd336
Stephen Connett <StephenCon@users.noreply.github.com>
2023-09-28 20:01:44 +0100

    added comments

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -1,25 +1,37 @@
+// Importing necessary React and React-Bootstrap components
 import React, { useState } from 'react';
 import { Navbar, NavDropdown, Nav } from 'react-bootstrap';
 import './NavigationBar.css';
+// Importing profile image from assets
 import profile_image from '../../assets/images/emptyprofile.png';
 
+// NavigationBar component definition
 const NavigationBar = () => {
+    // State to control whether the NavDropdown is shown or not
     const [show, setShow] = useState(false);
 
     return (
         <Navbar>
-            <Navbar.Brand href="#home">Meal Mixer</Navbar.Brand>
-            <Navbar.Toggle />
+            <Navbar.Brand href="#home">Meal Mixer</Navbar.Brand> {/* Brand Name of the Navbar */}
+            <Navbar.Toggle /> {/* Toggle button for smaller screen sizes */}
             <Navbar.Collapse className="justify-content-end p-1">
                 <Nav>
-                    <NavDropdown title={
-                        <img
-                            src={profile_image}
-                            alt="Profile"
-                            className="rounded-circle"
-                            width="65" height="65"
-                        />
-                    } show={show} id="basic-nav-dropdown" onMouseOver={() => setShow(true)} onMouseLeave={() => setShow(false)}>
+                    {/* NavDropdown component with an image as the title */}
+                    <NavDropdown
+                        title={
+                            <img
+                                src={profile_image} // Profile image
+                                alt="Profile" // Alt text for the image
+                                className="rounded-circle" // Rounded image style
+                                width="65" height="65"
+                            />
+                        }
+                        show={show} // Control the dropdown display based on state
+                        id="basic-nav-dropdown"
+                        onMouseOver={() => setShow(true)} // Show dropdown on mouse over
+                        onMouseLeave={() => setShow(false)} // Hide dropdown on mouse leave
+                    >
+                        {/* Dropdown items */}
                         <NavDropdown.Item href="#action/3.1">Login</NavDropdown.Item>
                         <NavDropdown.Item href="#action/3.2">Sign Up</NavDropdown.Item>
                     </NavDropdown>
@@ -29,4 +41,5 @@ const NavigationBar = () => {
     );
 };
 
+// Exporting the NavigationBar component
 export default NavigationBar;
