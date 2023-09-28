ab34b1b90ee71506506dcc9c3930a1e85619c010
Stephen Connett <StephenCon@users.noreply.github.com>
2023-09-28 19:58:33 +0100

    fixed image dropdown menu

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -10,20 +10,19 @@ const NavigationBar = () => {
         <Navbar>
             <Navbar.Brand href="#home">Meal Mixer</Navbar.Brand>
             <Navbar.Toggle />
-            <Navbar.Collapse className="justify-content-end">
+            <Navbar.Collapse className="justify-content-end p-1">
                 <Nav>
-                    <Nav.Link eventKey={2} onMouseOver={() => setShow(true)} onMouseLeave={() => setShow(false)}>
+                    <NavDropdown title={
                         <img
                             src={profile_image}
                             alt="Profile"
                             className="rounded-circle"
-                            width="50" height="50"
+                            width="65" height="65"
                         />
-                        <NavDropdown title="" show={show} id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Login</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Sign Up</NavDropdown.Item>
-                        </NavDropdown>
-                    </Nav.Link>
+                    } show={show} id="basic-nav-dropdown" onMouseOver={() => setShow(true)} onMouseLeave={() => setShow(false)}>
+                        <NavDropdown.Item href="#action/3.1">Login</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">Sign Up</NavDropdown.Item>
+                    </NavDropdown>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
