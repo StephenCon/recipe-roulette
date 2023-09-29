0bb6f5e56dd9767dadb0758ba9fc2fa372e0a5e1
Stephen Connett <StephenCon@users.noreply.github.com>
2023-09-29 09:41:50 +0100

    added add recipe + button

--- src/components/RecipeList/RecipeList.js
+++ src/components/RecipeList/RecipeList.js
@@ -1,21 +1,34 @@
 import React from 'react';
+import './RecipeList.css';
 
 const RecipeList = ({ recipes, onDeleteRecipe, onEditRecipe }) => {
     return (
-        <div className="container mt-5">
-            <h2 className="mb-4">Recipes</h2>
+        <div className="recipe-list">
+            {/* Title and Add Button */}
+            <div className="row align-items-center mb-4">
+                <div className="col">
+                    <h2>Recipes</h2>
+                </div>
+                <div className="col-auto">
+                    <button className="btn btn-primary">
+                        <span className="font-weight-bold">+</span>
+                    </button>
+                </div>
+            </div>
+
+            {/* Recipe List */}
             {recipes.length === 0 ? (
                 <p>No recipes added yet.</p>
             ) : (
-                <ul className="list-group">
+                <ul>
                     {recipes.map((recipe, index) => (
-                        <li key={index} className="list-group-item mb-3">
-                            <h3 className="mb-2">{recipe.name}</h3>
-                            <p className="mb-1"><strong>Ingredients:</strong> {recipe.ingredients.join(', ')}</p>
-                            <p className="mb-2"><strong>Instructions:</strong> {recipe.instructions}</p>
-                            <p className="mb-2"><strong>Meal Type:</strong> {recipe.mealType}</p>
-                            <button className="btn btn-warning btn-sm me-2" onClick={() => onEditRecipe(index)}>Edit</button>
-                            <button className="btn btn-danger btn-sm" onClick={() => onDeleteRecipe(index)}>Delete</button>
+                        <li key={index}>
+                            <h3>{recipe.name}</h3>
+                            <p><strong>Ingredients:</strong> {recipe.ingredients.join(', ')}</p>
+                            <p><strong>Instructions:</strong> {recipe.instructions}</p>
+                            <p><strong>Meal Type:</strong> {recipe.mealType}</p>
+                            <button onClick={() => onEditRecipe(index)}>Edit</button>
+                            <button onClick={() => onDeleteRecipe(index)}>Delete</button>
                         </li>
                     ))}
                 </ul>
