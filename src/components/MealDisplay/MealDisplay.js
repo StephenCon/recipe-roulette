8e530812bf44f184b4e0cb8aefa1bfea3a1cb7e0
Stephen Connett <StephenCon@users.noreply.github.com>
2023-09-29 11:36:47 +0100

    fixed seperator in card title

--- src/components/MealDisplay/MealDisplay.js
+++ src/components/MealDisplay/MealDisplay.js
@@ -6,7 +6,7 @@ const MealDisplay = ({ meals, recipes, onRandomize }) => {
     return (
         <div className="meal-display container mt-5 p-4 rounded">
             {/* Title and RandomiserButton on the same row */}
-            <div className="d-flex justify-content-between align-items-center mb-4">
+            <div className="d-flex justify-content-between align-items-center">
                 <h2>Today's Meals</h2>
                 {/* Adding RandomiserButton here */}
                 <RandomiserButton recipes={recipes} onRandomize={onRandomize} />
@@ -15,7 +15,7 @@ const MealDisplay = ({ meals, recipes, onRandomize }) => {
             <div className="row">
                 {/* Breakfast Section */}
                 <div className="col-md-4 mb-3">
-                    <div className="card bg-light gradient-card">
+                    <div className="card bg-light">
                         <div className="card-body text-center">
                             <h3 className="card-title">Breakfast</h3>
                             {meals.breakfast ? (
@@ -31,7 +31,7 @@ const MealDisplay = ({ meals, recipes, onRandomize }) => {
 
                 {/* Lunch Section */}
                 <div className="col-md-4 mb-3">
-                    <div className="card bg-light gradient-card">
+                    <div className="card bg-light">
                         <div className="card-body text-center">
                             <h3 className="card-title">Lunch</h3>
                             {meals.lunch ? (
@@ -47,7 +47,7 @@ const MealDisplay = ({ meals, recipes, onRandomize }) => {
 
                 {/* Dinner Section */}
                 <div className="col-md-4 mb-3">
-                    <div className="card bg-light gradient-card">
+                    <div className="card bg-light">
                         <div className="card-body text-center">
                             <h3 className="card-title">Dinner</h3>
                             {meals.dinner ? (
