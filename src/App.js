bb83c5cf40aa8fd3abd01dc1c4b8277a68b36287
Stephen Connett <StephenCon@users.noreply.github.com>
2023-09-29 09:59:32 +0100

    + button now adds new recipes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useState } from 'react';
-import RecipeForm from './components/RecipeForm/RecipeForm';
 import RecipeList from './components/RecipeList/RecipeList';
 import MealDisplay from './components/MealDisplay/MealDisplay';
 import RandomiserButton from './components/RandomiserButton/RandomiserButton';
@@ -9,54 +8,44 @@ import NavigationBar from './components/NavigationBar/NavigationBar';
 import Footer from './components/Footer/Footer';
 
 const App = () => {
-  // State to store the list of recipes
   const [recipes, setRecipes] = useState([]);
-
-  // State to store the randomized meals (breakfast, lunch, dinner)
   const [randomMeals, setRandomMeals] = useState({ breakfast: null, lunch: null, dinner: null });
 
-  // Handler to add a new recipe to the recipes list
   const handleAddRecipe = (newRecipe) => {
     setRecipes([...recipes, newRecipe]);
   };
 
-  // Handler to delete a recipe from the recipes list
   const handleDelete = (index) => {
     const updatedRecipes = [...recipes];
     updatedRecipes.splice(index, 1);
     setRecipes(updatedRecipes);
   };
 
-  // Handler to edit an existing recipe in the recipes list
   const handleEdit = (index, updatedRecipe) => {
     const updatedRecipes = [...recipes];
     updatedRecipes[index] = updatedRecipe;
     setRecipes(updatedRecipes);
   };
 
-  // Handler to update the state with randomized meals
   const handleRandomizedMeals = (randomizedMeals) => {
     setRandomMeals(randomizedMeals);
   };
 
   return (
     <div className="container">
-      {/* Navigation Bar  */}
       <NavigationBar />
       <hr className="hr" />
       <div className='container border rounded'>
-        {/* Display the randomized meals */}
         <MealDisplay meals={randomMeals} />
-        {/* Button to trigger the randomization of meals */}
         <RandomiserButton recipes={recipes} onRandomize={handleRandomizedMeals} />        
       </div>
-      {/* List display of all recipes with options to edit or delete */}
-      <RecipeList recipes={recipes} onDeleteRecipe={handleDelete} onEditRecipe={handleEdit} />
-      <hr className="hr" />
-      {/* Recipe form to add a new recipe */}
-      <RecipeForm onAddRecipe={handleAddRecipe} />
-      <hr class="hr" />
-      
+      <RecipeList 
+          recipes={recipes} 
+          onDeleteRecipe={handleDelete} 
+          onEditRecipe={handleEdit} 
+          onAddRecipe={handleAddRecipe}  // Passing the onAddRecipe prop
+      />
+      <hr className="hr" />      
       <Footer />
     </div>
   );
--- src/components/RecipeList/RecipeList.js
+++ src/components/RecipeList/RecipeList.js
@@ -1,22 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Modal, Button } from 'react-bootstrap';
+import RecipeForm from '../RecipeForm/RecipeForm';
 import './RecipeList.css';
 
-const RecipeList = ({ recipes, onDeleteRecipe, onEditRecipe }) => {
+const RecipeList = ({ recipes, onDeleteRecipe, onEditRecipe, onAddRecipe }) => {
+    const [showModal, setShowModal] = useState(false);
+
+    const handleFormSubmit = (newRecipe) => {
+        onAddRecipe(newRecipe);
+        setShowModal(false); // Close the modal after adding the recipe
+    };
+
     return (
         <div className="recipe-list">
-            {/* Title and Add Button */}
             <div className="row align-items-center mb-4">
                 <div className="col">
                     <h2>Recipes</h2>
                 </div>
                 <div className="col-auto">
-                    <button className="btn btn-primary">
+                    <Button variant="primary" onClick={() => setShowModal(true)}>
                         <span className="font-weight-bold">+</span>
-                    </button>
+                    </Button>
                 </div>
             </div>
 
-            {/* Recipe List */}
             {recipes.length === 0 ? (
                 <p>No recipes added yet.</p>
             ) : (
@@ -33,6 +40,15 @@ const RecipeList = ({ recipes, onDeleteRecipe, onEditRecipe }) => {
                     ))}
                 </ul>
             )}
+
+            <Modal show={showModal} onHide={() => setShowModal(false)}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Add Recipe</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <RecipeForm onAddRecipe={handleFormSubmit} />
+                </Modal.Body>
+            </Modal>
         </div>
     );
 };
