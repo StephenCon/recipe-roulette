71ac3334a6f67c13a328e895ea480e1906960889
Stephen Connett <StephenCon@users.noreply.github.com>
2023-10-02 12:30:45 +0100

    created and styled sign up form

---
+++ src/components/SignUpForm/SignUpForm.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import './SignUpForm.css'; // Import the CSS file
+
+const SignupForm = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const response = await fetch('http://localhost:3001/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username, password }),
+        });
+
+        if (response.status === 201) {
+            alert('User registered successfully');
+        } else {
+            alert('Registration failed');
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit} className="signup-form">
+            <div className="form-group">
+                <label>Username</label>
+                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="form-control" />
+            </div>
+            <div className="form-group">
+                <label>Password</label>
+                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="form-control" />
+            </div>
+            <button type="submit" className="btn btn-primary">Sign Up</button>
+        </form>
+    );
+};
+
+export default SignupForm;
