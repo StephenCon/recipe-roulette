9a3718c86f96157287eb3e4e3c55c970835958bc
Stephen Connett <StephenCon@users.noreply.github.com>
2023-10-02 12:30:25 +0100

    added sign up form modal

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -1,7 +1,9 @@
 // Importing necessary React and React-Bootstrap components
 import React, { useState } from 'react';
-import { Navbar, NavDropdown, Nav } from 'react-bootstrap';
+import { Navbar, NavDropdown, Nav, Modal } from 'react-bootstrap';
+import SignupForm from '../SignUpForm/SignUpForm'; // Import your SignupForm component
 import './NavigationBar.css';
+
 // Importing profile image from assets
 import profile_image from '../../assets/images/emptyprofile.png';
 
@@ -9,37 +11,47 @@ import profile_image from '../../assets/images/emptyprofile.png';
 const NavigationBar = () => {
     // State to control whether the NavDropdown is shown or not
     const [show, setShow] = useState(false);
+    // State to control the visibility of the Signup Modal
+    const [showSignupModal, setShowSignupModal] = useState(false);
 
     return (
         <Navbar className="bg-white">
-            <Navbar.Brand href="#home">Recipe Roulette</Navbar.Brand> {/* Brand Name of the Navbar */}
+            <Navbar.Brand href="#home">Recipe Roulette</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse className="justify-content-end p-1" id="basic-navbar-nav">
                 <Nav>
-                    {/* NavDropdown component with an image as the title */}
                     <NavDropdown
                         title={
                             <img
-                                src={profile_image} // Profile image
-                                alt="Profile" // Alt text for the image
-                                className="rounded-circle border border-white" // Rounded image style with a white border
-                                width="50" height="50"
+                                src={profile_image}
+                                alt="Profile"
+                                className="rounded-circle border border-white"
+                                width="50"
+                                height="50"
                             />
                         }
-                        show={show} // Control the dropdown display based on state
+                        show={show}
                         id="basic-nav-dropdown"
-                        onMouseOver={() => setShow(true)} // Show dropdown on mouse over
-                        onMouseLeave={() => setShow(false)} // Hide dropdown on mouse leave
+                        onMouseOver={() => setShow(true)}
+                        onMouseLeave={() => setShow(false)}
                     >
-                        {/* Dropdown items */}
                         <NavDropdown.Item href="#action/3.1">Login</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Sign Up</NavDropdown.Item>
+                        <NavDropdown.Item onClick={() => setShowSignupModal(true)}>Sign Up</NavDropdown.Item>
                     </NavDropdown>
                 </Nav>
             </Navbar.Collapse>
+
+            {/* Signup Modal */}
+            <Modal show={showSignupModal} onHide={() => setShowSignupModal(false)}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Sign Up</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <SignupForm />
+                </Modal.Body>
+            </Modal>
         </Navbar>
     );
 };
 
-// Exporting the NavigationBar component
 export default NavigationBar;
