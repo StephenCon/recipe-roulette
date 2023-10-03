d3065c1282ff0bb33989e17def444825316e4230
Stephen Connett <StephenCon@users.noreply.github.com>
2023-10-03 11:49:40 +0100

    changed class to className

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,14 +5,14 @@ const Footer = () => {
 
     return (
         <div className="container bg-white">
-            <footer class="py-3 my-4">
-                {/* <ul class="nav justify-content-center border-bottom pb-3 mb-3">
-                    <li class="nav-item"><a class="nav-link px-2 text-muted">About</a></li>
-                    <li class="nav-item"><a class="nav-link px-2 text-muted">Road Map</a></li>
-                    <li class="nav-item"><a class="nav-link px-2 text-muted">Random</a></li>
-                    <li class="nav-item"><a class="nav-link px-2 text-muted">FAQs</a></li>
+            <footer className="py-3 my-4">
+                {/* <ul className="nav justify-content-center border-bottom pb-3 mb-3">
+                    <li className="nav-item"><a className="nav-link px-2 text-muted">About</a></li>
+                    <li className="nav-item"><a className="nav-link px-2 text-muted">Road Map</a></li>
+                    <li className="nav-item"><a className="nav-link px-2 text-muted">Random</a></li>
+                    <li className="nav-item"><a className="nav-link px-2 text-muted">FAQs</a></li>
                 </ul> */}
-                <p class="text-center text-muted">© {currentYear} Recipe Roulette. All rights reserved.</p>
+                <p className="text-center text-muted">© {currentYear} Recipe Roulette. All rights reserved.</p>
             </footer>
         </div>
     );
