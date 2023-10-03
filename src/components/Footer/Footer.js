702d48159e5276ef8985e0e7d87edc24b9a6787b
Stephen Connett <StephenCon@users.noreply.github.com>
2023-10-03 11:27:40 +0100

    changed class to ClassName

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     const currentYear = new Date().getFullYear();
 
     return (
-        <div class="container bg-white">
+        <div className="container bg-white">
             <footer class="py-3 my-4">
                 {/* <ul class="nav justify-content-center border-bottom pb-3 mb-3">
                     <li class="nav-item"><a class="nav-link px-2 text-muted">About</a></li>
