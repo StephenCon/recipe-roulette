6b779b9440c5b8e48e983cce57a6942c04b16152
Stephen Connett <StephenCon@users.noreply.github.com>
2023-10-03 15:16:22 +0100

    imported components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import NavigationBar from '../components/NavigationBar/NavigationBar';
+import Hero from '../components/Hero/Hero';
+import Features from '../components/Feature/Feature';
+import HowItWorks from '../components/HowItWorks/HowItWorks';
+import Testimonials from '../components/Testimonials/Testimonials';
+import Footer from '../components/Footer/Footer';
 
 
 function App() {
     return (
         <div className="App">
             <NavigationBar />
+            <Hero />
+            <Features />
+            <HowItWorks />
+            <Testimonials />
+            <Footer />
         </div>
     );
 }
