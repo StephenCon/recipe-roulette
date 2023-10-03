da6eb61060f5d6e840dce79f51bf2d4f5d38f420
Stephen Connett <StephenCon@users.noreply.github.com>
2023-10-03 15:29:30 +0100

    added landing page files

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import NavigationBar from '../components/NavigationBar/NavigationBar';
-import HeroSection from '../components/HeroSection/HeroSection';
+/* import HeroSection from '../components/HeroSection/HeroSection';
 import Features from '../components/Feature/Feature';
 import HowItWorks from '../components/HowItWorks/HowItWorks';
 import Testimonials from '../components/Testimonials/Testimonials';
-import Footer from '../components/Footer/Footer';
+import Footer from '../components/Footer/Footer'; */
 
 
 function App() {
     return (
         <div className="App">
             <NavigationBar />
-            <HeroSection />
+            {/* <HeroSection />
             <Features />
             <HowItWorks />
             <Testimonials />
-            <Footer />
+            <Footer /> */}
         </div>
     );
 }
