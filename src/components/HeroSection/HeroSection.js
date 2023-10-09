71ed3f1c6f0228b584dd2a5047a6a8ac192b54bf
Stephen Connett <StephenCon@users.noreply.github.com>
2023-10-09 17:33:33 +0100

    centered the text

--- src/components/HeroSection/HeroSection.js
+++ src/components/HeroSection/HeroSection.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
 const HeroSection = () => {
-
     return (
-        <div>
-            <div>
-                <h1>Welcome to Recipe Roulette!</h1>
-                <p>Find the best recipes for your next meal.</p>
-                <button>Get Started</button>
+        <div className="text-center py-5">
+            <div className="container">
+                <h1 className="display-4">Welcome to Recipe Roulette!</h1>
+                <p className="lead">Find the best recipes for your next meal.</p>
+                <button className="btn btn-secondary btn-lg">Get Started</button>
             </div>
         </div>
     );
