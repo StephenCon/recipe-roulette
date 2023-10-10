86e5bb354f2f91ed9adec39d40eab827be273711
Stephen Connett <StephenCon@users.noreply.github.com>
2023-10-10 14:42:49 +0100

    removed .env variables due to errors

--- server/server.js
+++ server/server.js
@@ -1,18 +1,18 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
 const session = require('express-session');
-require('dotenv').config();
+const bcrypt = require('bcrypt');
 
 const app = express();
 const port = 3001;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
 app.use(session({
-  secret: process.env.SESSION_SECRET || 'default-secret',
+  secret: '7b8b965ad4bca0e41ab51de7b31363a21c4d75a60dfa144d16c4266ce60b5f17',
   resave: false,
   saveUninitialized: true,
   cookie: {
@@ -22,22 +22,20 @@ app.use(session({
   }
 }));
 
-// MongoDB connection string
-const uri = process.env.MONGODB_URI;
+const uri = 'mongodb+srv://stephen:Mark123%21%22%C2%A3@cluster0.dcxcg2q.mongodb.net/';
 
-// Connect to MongoDB Atlas
 MongoClient.connect(uri)
   .then(client => {
     console.log('Connected to MongoDB Atlas');
-
     const db = client.db('recipe-roulette');
     const usersCollection = db.collection('users');
 
-    // Define the /register route
     app.post('/register', async (req, res) => {
       const { username, password } = req.body;
+      // Hash the password before storing (if you add bcrypt)
+      // const hashedPassword = await bcrypt.hash(password, 10);
       try {
-        await usersCollection.insertOne({ username, password });
+        await usersCollection.insertOne({ username, password /* hashedPassword if using bcrypt */ });
         res.status(201).send('User registered successfully');
       } catch (error) {
         console.error(error);
@@ -45,7 +43,6 @@ MongoClient.connect(uri)
       }
     });
 
-    // Define the /login route
     app.post('/login', async (req, res) => {
       const { username, password } = req.body;
       try {
@@ -53,10 +50,12 @@ MongoClient.connect(uri)
         if (!user) {
           return res.status(400).send('User not found');
         }
-        if (user.password !== password) {
+        // If using bcrypt, compare the hashed password
+        // const match = await bcrypt.compare(password, user.password);
+        if (user.password !== password /* match if using bcrypt */) {
           return res.status(400).send('Invalid password');
         }
-        req.session.userId = user._id; // Store user ID in session
+        req.session.userId = user._id;
         res.status(200).send('Logged in successfully');
       } catch (error) {
         console.error('Error during login:', error);
@@ -64,17 +63,6 @@ MongoClient.connect(uri)
       }
     });
 
-    // Define the /logout route
-    app.post('/logout', (req, res) => {
-      req.session.destroy(err => {
-        if (err) {
-          return res.status(500).send('Could not log out.');
-        }
-        res.status(200).send('Logged out successfully');
-      });
-    });
-
-    // Start the server
     app.listen(port, () => {
       console.log(`Server running at http://localhost:${port}/`);
     });
