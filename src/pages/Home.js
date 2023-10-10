1b6299a93e23020ee1258320523bff448f8b5d82
Stephen Connett <StephenCon@users.noreply.github.com>
2023-10-10 11:29:28 +0100

    added container

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,13 +8,16 @@ import Footer from '../components/Footer/Footer';
 
 function App() {
     return (
-        <div className="App">
+        <div className="container">
             <NavigationBar />
+            <hr className='hr' />
+            <div className='container'>
             <HeroSection />
             <Features />
             <HowItWorks />
             <Testimonials />
             <Footer />
+            </div>
         </div>
     );
 }
