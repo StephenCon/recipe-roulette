47d8b42c10aea007f624e6ff4348ac570721e95f
Stephen Connett <StephenCon@users.noreply.github.com>
2023-10-13 13:29:36 +0100

    removed session code

--- server/server.js
+++ server/server.js
@@ -3,35 +3,14 @@ const express = require('express');
 const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
 const bcrypt = require('bcrypt');
-const session = require('express-session');
-const RedisStore = require('connect-redis')(session);
-const redis = require('redis');
 
 const app = express();
 const port = 3001;
 
 const uri = 'mongodb+srv://stephen:Mark123%21%22%C2%A3@cluster0.dcxcg2q.mongodb.net/';
 
-// Initialize Redis client
-const redisClient = redis.createClient({
-  host: 'localhost',
-  port: 6379
-});
-
 app.use(cors());
 app.use(express.json());
-
-app.use(session({
-  store: new RedisStore({ client: redisClient }),
-  secret: '7b8b965ad4bca0e41ab51de7b31363a21c4d75a60dfa144d16c4266ce60b5f17',
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-    secure: process.env.NODE_ENV === 'production',
-    httpOnly: true,
-    maxAge: 1000 * 60 * 60 * 24 // 1 day
-  }
-}));
 
 MongoClient.connect(uri)
   .then(client => {
@@ -58,21 +37,11 @@ MongoClient.connect(uri)
         if (!user || !(await bcrypt.compare(password, user.password))) {
           return res.status(400).send('Invalid username or password');
         }
-        req.session.userId = user._id;
         res.status(200).redirect('/dashboard');
       } catch (error) {
         console.error('Error during login:', error);
         res.status(500).send('Internal Server Error');
       }
-    });
-
-    app.post('/logout', (req, res) => {
-      req.session.destroy(err => {
-        if (err) {
-          return res.status(500).send('Logout failed');
-        }
-        res.redirect('/login');
-      });
     });
 
     app.listen(port, () => {
