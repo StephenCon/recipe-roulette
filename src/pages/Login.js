7b574e922058b0e7ccc0067c1d84e02ec1680356
Stephen Connett <StephenCon@users.noreply.github.com>
2023-10-19 08:30:53 +0100

    sorted babel package

---
+++ src/pages/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoginForm from '../components/LoginForm/LoginForm';
+
+function App() {
+    return (
+        <div className="container">
+            <LoginForm />
+        </div>
+    );
+}
+
+export default App;
