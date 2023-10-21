1dc6d8d2ae4ecd39ded1672239a54bfb7fdb12ea
Stephen Connett <StephenCon@users.noreply.github.com>
2023-10-21 16:10:19 +0100

    Refactor and comment App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
-// App.js
+// External imports
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from './pages/Home';
-import Dashboard from './pages/Dashboard';  // Ensure Dashboard import is correct
-import '../src/App.css';
 
+// Internal imports
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import LoginForm from './components/LoginForm/LoginForm';
+import './App.css';  // Simplified the import path as it's relative to the current file
+
+/**
+ * Main application component.
+ * Sets up routing for the application.
+ */
 function App() {
     return (
         <div className="App">
+            {/* Set up client-side routing using BrowserRouter */}
             <BrowserRouter>
+                {/* Define application routes and corresponding components */}
                 <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/dashboard" element={<Dashboard />} />  {/* No change needed here */}
+                    <Route path="/" element={<Home />} />         {/* Home route */}
+                    <Route path="/dashboard" element={<Dashboard />} /> {/* Dashboard route */}
+                    <Route path="/login" element={<LoginForm />} />     {/* Login route */}
                 </Routes>
             </BrowserRouter>
         </div>
     );
 }
 
+// Export App component for use in other parts of the application
 export default App;
