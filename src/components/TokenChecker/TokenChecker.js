463e6858da33ea17225b338edbfd08be793a68cd
Stephen Connett <StephenCon@users.noreply.github.com>
2023-11-16 10:00:40 +0000

    commented

--- src/components/TokenChecker/TokenChecker.js
+++ src/components/TokenChecker/TokenChecker.js
@@ -1,34 +1,28 @@
+// External imports
 import { useEffect } from 'react';
-import { useHistory } from 'react-router-dom';
-import { isTokenExpired } from '../utils/auth';
+import { useNavigate } from 'react-router-dom';
+import { isTokenExpired } from '../../utils/auth';
 
-const TokenChecker = () => {
-    const history = useHistory();
+/**
+ * TokenChecker component checks the expiration status of the user's token.
+ * Redirects to the login page if the token is expired.
+ */
+function TokenChecker() {
+    const navigate = useNavigate();
 
     useEffect(() => {
-        const checkTokenStatus = () => {
-            const token = localStorage.getItem('token');
+        // Retrieve the user's token from local storage
+        const token = localStorage.getItem('token');
 
-            if (isTokenExpired(token)) {
-                // Redirect to the login page or perform other actions
-                history.push('/login');
-            }
-        };
+        // Check if the token is expired
+        if (isTokenExpired(token)) {
+            // Redirect to the login page if the token is expired
+            navigate('/');
+        }
+    }, [navigate]);
 
-        // Check token status on component mount
-        checkTokenStatus();
-
-        // Optionally, you can set up a timer to periodically check the token status
-        const tokenCheckInterval = setInterval(() => {
-            checkTokenStatus();
-        }, 60000); // Check every 60 seconds
-
-        // Clear the interval on component unmount to avoid memory leaks
-        return () => clearInterval(tokenCheckInterval);
-    }, [history]);
-
-    // This component doesn't render anything, as it's mainly used for side effects
+    // This component doesn't render anything
     return null;
-};
+}
 
 export default TokenChecker;
